c4a21442bf77d4d9467806c9d4c2dd9d39348f46
roobon <roobon@gmail.com>
2022-11-06 19:05:59 +0600

    CRUD completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormValidationComponent } from './form-validation/form-validation.component';
+
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    HomeComponent, AppComponent, UsersComponent
+    HomeComponent, AppComponent, UsersComponent, FormValidationComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule, AppRoutingModule
+    BrowserModule, HttpClientModule, FormsModule, AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,10 +16,30 @@ export class UsersComponent implements OnInit {
     })
   }
   userCreate(form:any){
-    this.userService.createUser(form.value).subscribe((user: User[])=>{console.log("User created ", user);
+    this.userService.createUser(form.value).subscribe((user: User)=>{console.log("User created ", user);
+    this.userService.readUsers().subscribe((users: User[])=>{
+      this.users = users;
+    })
   })
   }
-
+  userUpdate(form:any){
+    form.value.id = this.selectedUser.id;
+    this.userService.updateUser(form.value).subscribe((user: User)=>{console.log("User updated ", user);
+    this.userService.readUsers().subscribe((users: User[])=>{
+      this.users = users;
+    })
+  })
+  }
+  selectUser(user: User){
+    this.selectedUser = user;
+  }
+  deleteUser(id:any){
+    this.userService.deleteUser(id).subscribe((user: User)=>{console.log("User Deleted ", user);
+    this.userService.readUsers().subscribe((users: User[])=>{
+      this.users = users;
+    })
+  })
+  }
   ngOnInit(): void {
   }
 
